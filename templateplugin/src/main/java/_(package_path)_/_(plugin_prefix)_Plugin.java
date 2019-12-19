238ec41d22b2b60419d0de99068af65de45cccb8
TheStonedTurtle <mgoodwin559@gmail.com>
2019-12-18 18:26:48 -0800

    Update template file import order

--- templateplugin/src/main/java/_(package_path)_/_(plugin_prefix)_Plugin.java
+++ templateplugin/src/main/java/_(package_path)_/_(plugin_prefix)_Plugin.java
@@ -2,15 +2,15 @@ package ${package};
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @PluginDescriptor(
