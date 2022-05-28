4a0a450e580cb58b1d93ced9ea15602505a4c9be
Ferrariic <thearcticfalcon@gmail.com>
2022-05-27 23:28:07 -0600

    Refactors create_new_plugin.py
    + Refactors create_new_plugin.py
    + Adds autopep8 formatting
    + Import sort
    + Adds comments for easy future maintenance
    + Adds organization in the way of basic formulas.
    + Reduces unnecessary redundancies

--- create_new_plugin.py
+++ create_new_plugin.py
@@ -25,174 +25,239 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
+import argparse
 import os
+import re
 import sys
 from collections import OrderedDict
-import argparse
-import re
+from pyclbr import Function
 from string import Template
 
-templatedir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "templateplugin")
-
-def strip_plugin(str):
-	return re.sub(r"(?i)[ _-]*plugin$", "", str)
-
-def reformat(str, replacer):
-	if replacer != to_spaces:
-		str=re.sub(r"[^a-zA-Z0-9_ -]", "", str)
-	if re.match(r".*[ _-]", str):
-		return re.sub(r"(?:^|[ _.-]+)([^ _.-]+)", lambda m:replacer(m.group(1)), str).strip(" -_")
-	return re.sub(r"((?:^.|[A-Z0-9]+)(?:[a-z0-9]+|$))", lambda m:replacer(m.group(1)), str).strip(" -_")
-
-def to_spaces(seg):
-	return " " + seg.capitalize()
-
-def to_camelcase(seg):
-	return seg.capitalize()
-
-def to_dashes(seg):
-	return "-" + seg.lower()
-
-def to_lowercase(seg):
-	return seg.lower()
-
-def strfun(strfun):
-	if isinstance(strfun, str):
-		return strfun
-	return strfun()
-
-subs = OrderedDict([
-	("name", {
-		"ask": True,
-		"desc": "The name your plugin will be shown by in menus",
-		"value": "Foo Bazzer",
-		"strip_plugin": True,
-	}),
-	("package", {
-		"ask": True,
-		"desc": "The java package your plugin will be in. Typically a reversed domain name",
-		"value": "com.example.foobazzer",
-		"strip_plugin": True,
-	}),
-	("author", {
-		"ask": True,
-		"desc": "Who wrote/maintains this plugin",
-		"value": "John Doe",
-	}),
-	("description", {
-		"ask": True,
-		"desc": "A short string describing the plugin",
-		"value": "Adds a bazzer to the foo",
-	}),
-	("version", {
-		"desc": "The initial version number of the plugin",
-		"value": "1.0-SNAPSHOT",
-	}),
-	("plugin_prefix", {
-		"desc": "The name of the your plugin's main class, without the 'Plugin' suffix",
-		"value": lambda: reformat(subs["name"]["value"], to_camelcase),
-		"strip_plugin": True
-	}),
-	("artifact_id", {
-		"desc": "The name of the maven artifact",
-		"value": lambda: reformat(subs["name"]["value"], to_dashes),
-	}),
-	("group_id", {
-		"desc": "The group of the maven artifact",
-		"value": lambda: subs["package"]["value"],
-	}),
-	("plugin_config_group", {
-		"desc": "The prefix used to store config keys",
-		"value": lambda: reformat(subs["name"]["value"], to_lowercase),
-		"strip_plugin": True
-	})
-])
-
+templatedir = os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), "templateplugin")
 pwd = os.getcwd()
+parser = argparse.ArgumentParser()
+
+
+def strip_plugin(string: str) -> str:
+    re_strip_plugin = r"(?i)[ _-]*plugin$"
+    return re.sub(pattern=re_strip_plugin, repl="", string=string)
+
+
+def reformat(string: str, string_replacement: Function) -> str:
+
+    if string_replacement != to_spaces:
+        pattern = r"[^a-zA-Z0-9_ -]"
+        string = re.sub(pattern=pattern, repl="", string=string)
+
+    if re.match(r".*[ _-]", string):
+        pattern = r"(?:^|[ _.-]+)([^ _.-]+)"
+        return re.sub(pattern=pattern, repl=lambda m: string_replacement(m.group(1)), string=string).strip(" -_")
+
+    pattern = r"((?:^.|[A-Z0-9]+)(?:[a-z0-9]+|$))"
+    return re.sub(pattern=pattern, repl=lambda m: string_replacement(m.group(1)), string=string).strip(" -_")
+
+
+def to_spaces(string: str) -> str:
+    return " " + string.capitalize()
+
+
+def to_camelcase(string: str) -> str:
+    return string.capitalize()
+
+
+def to_dashes(string: str) -> str:
+    return "-" + string.lower()
+
+
+def to_lowercase(string: str) -> str:
+    return string.lower()
+
+
+def string_function(strfun):
+    if isinstance(strfun, str):
+        return strfun
+    return strfun()
+
+
+def entry_complete() -> bool:
+    """ Requests verification from the user for the entered fields """
+    i = input("Is the information above correct? [y/n] ").lower()
+    if i == "Y" or i == "y" or i == "":
+        return True
+    if i == "N" or i == "n":
+        return False
+
+
+ordered_steps = OrderedDict(
+    [
+        (
+            "name",
+            {
+                "ask": True,
+                "desc": "The name your plugin will be shown by in menus",
+                "value": "Foo Bazzer",
+                "strip_plugin": True,
+            },
+        ),
+        (
+            "package",
+            {
+                "ask": True,
+                "desc": "The java package your plugin will be in. Typically a reversed domain name",
+                "value": "com.example.foobazzer",
+                "strip_plugin": True,
+            },
+        ),
+        (
+            "author",
+            {
+                "ask": True,
+                "desc": "Who wrote/maintains this plugin",
+                "value": "John Doe",
+            },
+        ),
+        (
+            "description",
+            {
+                "ask": True,
+                "desc": "A short string describing the plugin",
+                "value": "Adds a bazzer to the foo",
+            },
+        ),
+        (
+            "version",
+            {
+                "desc": "The initial version number of the plugin",
+                "value": "1.0-SNAPSHOT",
+            },
+        ),
+        (
+            "plugin_prefix",
+            {
+                "desc": "The name of the your plugin's main class, without the 'Plugin' suffix",
+                "value": lambda: reformat(ordered_steps["name"]["value"], to_camelcase),
+                "strip_plugin": True,
+            },
+        ),
+        (
+            "artifact_id",
+            {
+                "desc": "The name of the maven artifact",
+                "value": lambda: reformat(ordered_steps["name"]["value"], to_dashes),
+            },
+        ),
+        (
+            "group_id",
+            {
+                "desc": "The group of the maven artifact",
+                "value": lambda: ordered_steps["package"]["value"],
+            },
+        ),
+        (
+            "plugin_config_group",
+            {
+                "desc": "The prefix used to store config keys",
+                "value": lambda: reformat(ordered_steps["name"]["value"], to_lowercase),
+                "strip_plugin": True,
+            },
+        ),
+    ]
+)
+
+
+def query_user(askAll: bool = True, ordered_steps: dict = ordered_steps) -> dict:
+    """ Queries user for ordered steps """
+    for key, var in ordered_steps.items():
+        if askAll or ("ask" in var and var["ask"]):
+            print(var["desc"])
+            print("[" + string_function(var["value"]) + "]")
+            val = input(key + ": ")
+            if val:
+                if "strip_plugin" in var and var["strip_plugin"]:
+                    val = strip_plugin(val)
+                var["value"] = val
+
+    print(" ")
+    for key, var in ordered_steps.items():
+        print('{} = "{}"'.format(key, string_function(var["value"])))
+    return ordered_steps
+
+
+# checks active directory
 pwdIsEmpty = len(os.listdir(pwd)) == 0
 if pwdIsEmpty:
-	subs["name"]["value"] = strip_plugin(reformat(os.path.basename(pwd), to_spaces))
+    ordered_steps["name"]["value"] = strip_plugin(
+        reformat(os.path.basename(pwd), to_spaces))
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--noninteractive", dest = "noninteractive", action='store_true')
-parser.add_argument("--output_directory", dest = "output_directory")
-for key, var in subs.items():
-	parser.add_argument("--" + key, dest = key, help = var["desc"])
+# set up parser arguments
+parser.add_argument("--noninteractive",
+                    dest="noninteractive", action="store_true")
+parser.add_argument("--output_directory", dest="output_directory")
+for key, var in ordered_steps.items():
+    parser.add_argument("--" + key, dest=key, help=var["desc"])
 args = vars(parser.parse_args())
 
+# sets run as noninteractive and removes ordered steps
 noninteractive = args["noninteractive"]
-
 if noninteractive and pwdIsEmpty:
-	subs["name"]["ask"] = False
+    ordered_steps["name"]["ask"] = False
 
-for key, var in subs.items():
-	if args[key] != None:
-		val = args[key]
-		if "strip_plugin" in var and var["strip_plugin"]:
-			val = strip_plugin(val)
-		var["value"] = val
-		var["ask"] = False
+for key, var in ordered_steps.items():
+    if args[key] == None:
+        continue
+    val = args[key]
+    if "strip_plugin" in var and var["strip_plugin"]:
+        val = strip_plugin(val)
+    var["ask"], var["value"] = False, val
 
-askAll = False
-while True:
-	for key, var in subs.items():
-		if askAll or ("ask" in var and var["ask"]):
-			if noninteractive:
-				print("\"{}\" was not specified in noninteractive mode".format(key))
-				sys.exit(1)
-			print(var["desc"])
-			print("[" + strfun(var["value"]) + "]")
-			val = input(key + ": ")
-			if val:
-				if "strip_plugin" in var and var["strip_plugin"]:
-					val = strip_plugin(val)
-				var["value"] = val
+# queries user for form data
+if not noninteractive:
+    ordered_steps = query_user(askAll=False)
+    while True:
+        if entry_complete():
+            break
+        ordered_steps = query_user(askAll=True)
 
-	askAll = True
-
-	print("")
-	for key, var in subs.items():
-		print("{} = \"{}\"".format(key, strfun(var["value"])))
-	def input_yes():
-		while True:
-			inp = input("Is this ok? [y/n] ").lower()
-			if inp == "Y" or inp == "y" or inp == "":
-				return True
-			if inp == "N" or inp == "n":
-				return False
-	if noninteractive or input_yes():
-		break
-
+# set output directory
 outdir = args["output_directory"]
 if outdir == None:
-	if pwdIsEmpty:
-		outdir = pwd
-	else:
-		outdir = os.path.join(pwd, strfun(subs["artifact_id"]["value"]))
+    if pwdIsEmpty:
+        outdir = pwd
+    else:
+        outdir = os.path.join(pwd, string_function(
+            ordered_steps["artifact_id"]["value"]))
 
+# set mappings
 mappings = {}
-for key, var in subs.items():
-	mappings[key] = strfun(var["value"])
+for key, var in ordered_steps.items():
+    mappings[key] = string_function(var["value"])
 mappings["package_path"] = mappings["package"].replace(".", os.path.sep)
-with open(os.path.join(templatedir, "../runelite.version"), "rt") as fi:
-	mappings["runelite_version"] = fi.read().strip()
 
+# set runelite version
+with open(os.path.join(templatedir, "../runelite.version"), "rt") as runelite_version:
+    mappings["runelite_version"] = runelite_version.read().strip()
+
+# convert form data into plugin template
 for root, dir, files in os.walk(templatedir):
-	for file in files:
-		try:
-			infi = os.path.join(root, file)
-			outfi = os.path.join(os.path.relpath(root, templatedir), file)
-			outfi = outfi.replace("_(", "${").replace(")_", "}")
-			outfi = Template(outfi).substitute(mappings)
-			outfi = os.path.join(outdir, outfi)
-			os.makedirs(os.path.dirname(outfi), exist_ok=True)
-			with open(infi, "rb") as ifd: # we need binary mode to not do that stupid crlf bullshit on windows
-				if file.endswith(".jar") or file.startswith("gradlew"):
-					with open(outfi, "wb") as ofd:
-						ofd.write(ifd.read())
-				else:
-					contents = Template(ifd.read().decode("utf-8")).substitute(mappings)
-					with open(outfi, "wb") as ofd:
-						ofd.write(contents.encode("utf-8"))
-		except ValueError as ex:
-			raise ValueError(infi) from ex
+    for file in files:
+        try:
+            infi = os.path.join(root, file)
+            outfi = os.path.join(os.path.relpath(root, templatedir), file)
+            outfi = outfi.replace("_(", "${").replace(")_", "}")
+            outfi = Template(outfi).substitute(mappings)
+            outfi = os.path.join(outdir, outfi)
+            os.makedirs(os.path.dirname(outfi), exist_ok=True)
+            # we need binary mode to not do that stupid crlf bullshit on windows
+            with open(infi, "rb") as ifd:
+                if file.endswith(".jar") or file.startswith("gradlew"):
+                    with open(outfi, "wb") as ofd:
+                        ofd.write(ifd.read())
+                else:
+                    contents = Template(ifd.read().decode(
+                        "utf-8")).substitute(mappings)
+                    with open(outfi, "wb") as ofd:
+                        ofd.write(contents.encode("utf-8"))
+        except ValueError as ex:
+            raise ValueError(infi) from ex
+ 
