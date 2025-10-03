c02bf815cd994bb4dd6d6127d522077962c24415
sudodevdante <sudodevdante@proton.me>
2025-10-03 20:47:54 +0200

    Fix build.gradle.kts - simplify configuration and use implementation for dependencies

--- plugins/clansplit-lootkeys/build.gradle.kts
+++ plugins/clansplit-lootkeys/build.gradle.kts
@@ -4,21 +4,7 @@ project.extra["PluginName"] = "ClanSplit Loot Keys"
 project.extra["PluginDescription"] = "Automatically track and send PvP Loot Keys to ClanSplit sessions"
 
 dependencies {
-    compileOnly("com.squareup.okhttp3:okhttp:4.12.0")
-    compileOnly("com.google.code.gson:gson:2.11.0")
-}
-
-tasks {
-    jar {
-        manifest {
-            attributes(mapOf(
-                "Plugin-Version" to project.version,
-                "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
-                "Plugin-Provider" to project.extra["PluginProvider"],
-                "Plugin-Description" to project.extra["PluginDescription"],
-                "Plugin-License" to project.extra["PluginLicense"]
-            ))
-        }
-    }
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.google.code.gson:gson:2.11.0")
 }
 
