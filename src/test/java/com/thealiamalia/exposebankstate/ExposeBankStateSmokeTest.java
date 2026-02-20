ef6942bf939a4d021165a00aaf2fa651e5b5664c
aliamaliam2fbusiness <aliamaliam2fbusiness@gmail.com>
2026-02-20 12:37:57 -0600

    Add smoke test for Gradle compatibility

---
+++ src/test/java/com/thealiamalia/exposebankstate/ExposeBankStateSmokeTest.java
@@ -0,0 +1,13 @@
+﻿package com.thealiamalia.exposebankstate;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ExposeBankStateSmokeTest
+{
+  @Test
+  public void smoke()
+  {
+    assertTrue(true);
+  }
+}
